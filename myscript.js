04330c300af44d07a5c2f683e0190e9b170b19b3
Chuku778 <joychioma777@gmail.com>
2024-10-09 12:46:25 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,42 @@
+document.write("Hello World")
+//For addition//
+var cats = 40
+var dogs = 35
+let animals = cats + dogs
+document.write("<br>")
+document.write(animals)
+function addition(cats,dogs) {
+    return cats + dogs;
+    }
+    // For subtraction//
+var igbo =  50
+var yoruba = 40
+var hausa = 100
+let fulani = igbo - yoruba
+document.write("<br>")
+ document.write(fulani)
+ function subtraction(igbo,yoruba) {
+return igbo - fulani;
+ }
+
+ // For multiplication //
+
+ let rice = 13
+ let beans = 5
+ let food= rice * beans
+ document.write("<br>")
+ document.write(food)
+function multiplication(rice,beans) { 
+    return rice * beans;
+}
+
+// For division //
+
+let pepper = 20
+let tomato = 4
+let vegetables= pepper / tomato 
+document.write("<br>")
+document.write(vegetables)
+function division(pepper,tomato) { return pepper / tomato;
+    
+}
